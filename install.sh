deb3479ab9bba888a6b5b523c150711a73db5c87
Tuomas Kareinen <tkareine@gmail.com>
2017-05-08 15:16:21 +0300

    IDEA: relocate VM options file for IDEA 2017.1

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ INSTALL_BY_SYMLINKING_DARWIN=(
     .osx
     bin/emacs
     bin/emacsclient
-    Library/Preferences/IdeaIC2016.3/idea.vmoptions
+    Library/Preferences/IdeaIC2017.1/idea.vmoptions
 )
 
 print_error() {
