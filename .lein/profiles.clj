688363229189bae8c5046a31b95ff9c5bb174f37
Tuomas Kareinen <tkareine@gmail.com>
2017-10-02 09:46:40 +0300

    lein: rely on stable CIDER version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{:repl {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]]
+{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
  :user {:plugins [[lein-pprint "1.1.2"]]}}
