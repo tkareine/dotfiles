0d282b03453200058e4c3959b980e8a1edc4dca7
Tuomas Kareinen <tkareine@gmail.com>
2017-12-07 12:42:47 +0200

    lein: update lein-pprint plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:repl {:plugins [[cider/cider-nrepl "0.15.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
- :user {:plugins [[lein-pprint "1.1.2"]]}}
+ :user {:plugins [[lein-pprint "1.2.0"]]}}
