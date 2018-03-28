0778b64503bf1fcb94c27da1e3a3301059cdc081
Tuomas Kareinen <tkareine@gmail.com>
2018-03-28 10:36:49 +0300

    Upgrade IntelliJ IDEA

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ INSTALL_BY_SYMLINKING_DARWIN=(
     .macos
     bin/emacs
     bin/emacsclient
-    Library/Preferences/IdeaIC2017.3/idea.vmoptions
+    Library/Preferences/IdeaIC2018.1/idea.vmoptions
 )
 
 print_error() {
