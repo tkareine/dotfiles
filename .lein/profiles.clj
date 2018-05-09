3553c08030bb80c03bb19bebe3f5fef61839ee98
Tuomas Kareinen <tkareine@gmail.com>
2018-05-09 09:16:46 +0300

    lein: update version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{:repl {:plugins [[cider/cider-nrepl "0.16.0"]]
+{:repl {:plugins [[cider/cider-nrepl "0.17.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
  :user {:plugins [[lein-pprint "1.2.0"]]}}
