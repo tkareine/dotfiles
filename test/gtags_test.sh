b281f561f9f678662658d433569b6b47490b1041
Tuomas Kareinen <tkareine@gmail.com>
2019-12-13 14:56:05 +0200

    ctags: find YAML anchor symbols

--- test/gtags_test.sh
+++ test/gtags_test.sh
@@ -117,6 +117,8 @@ SYMBOLS_GLOBAL_SHOULD_FIND_ONE=(
     yaml_ansible_register_key_plain_value_double_quoted
     yaml_ansible_register_key_single_quoted_value_plain
     yaml_ansible_register_key_double_quoted_value_plain
+    yaml_anchor-map
+    yaml_anchor-list_elem
 )
 
 SYMBOLS_GLOBAL_SHOULD_FIND_ZERO=(
