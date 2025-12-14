18d31d7aeed7925169232ffb21274979fc80bb5e
Tuomas Kareinen <tkareine@gmail.com>
2025-12-14 15:40:48 +0200

    bash: test that bash invocation outputs no errors

--- test/bash-test.sh
+++ test/bash-test.sh
@@ -14,11 +14,19 @@ __clean_shell() {
 }
 
 test_interactive_login_shell() {
-    __clean_shell --login -i -c true || fail_test "error in bash init for interactive login shell"
+    local stdout
+    stdout=$(__clean_shell --login -i -c true 2>&1)
+    local status=$?
+    [[ $status = 0 ]] || fail_test "error in bash init for interactive login shell"
+    assert_equal '' "$stdout"
 }
 
 test_interactive_nonlogin_shell() {
-    __clean_shell -i -c true || fail_test "error in bash init for interactive non-login shell"
+    local stdout
+    stdout=$(__clean_shell -i -c true 2>&1)
+    local status=$?
+    [[ $status = 0 ]] || fail_test "error in bash init for interactive non-login shell"
+    assert_equal '' "$stdout"
 }
 
 TEST_SOURCE=$0 source test/support/suite.sh
