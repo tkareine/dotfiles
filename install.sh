92b27d3c49bb70e2debebcac367753ad3e549f18
Tuomas Kareinen <tkareine@gmail.com>
2026-05-14 14:10:45 +0300

    install: install any symlinks

--- install.sh
+++ install.sh
@@ -64,6 +64,12 @@ INSTALL_DOTFILE_BY_SYMLINKING_DARWIN=(
     Library/Thunderbird/profiles.ini.example
 )
 
+# shellcheck disable=2034
+declare -A INSTALL_SYMLINKS_DARWIN=(
+    ["$HOME/.nodes/node-26"]=/opt/homebrew/opt/node@26
+    ["$HOME/.rubies/ruby-4"]=/opt/homebrew/opt/ruby@4
+)
+
 print_error() {
     echo "$@" >&2
 }
@@ -78,6 +84,39 @@ Install dotfiles by copying or symlinking to user's home directory.
 EOF
 }
 
+install_copy() {
+    local source=$1 destination=$2
+
+    [[ $# -le 1 || -z $source || -z $destination ]] && print_error "install_copy(): expects source and destination as parameters" && return 1
+
+    if [[ -f $destination ]] && cmp -s "$source" "$destination"; then
+        echo "installed already, skipping: $destination"
+    elif [[ ! -e $destination || $FORCE_INSTALL ]]; then
+        echo "copying: $destination -> $source"
+        mkdir -p "$(dirname "$destination")"
+        rm -f "$destination" # Delete potential destination file that is a symlink to avoid following it
+        cp "$source" "$destination"
+    else
+        echo "exists already, skipping: $destination"
+    fi
+}
+
+install_symlink() {
+    local source=$1 destination=$2
+
+    [[ $# -le 1 || -z $source || -z $destination ]] && print_error "install_symlink(): expects source and destination as parameters" && return 1
+
+    if [[ -L $destination && $source -ef $destination ]]; then
+        echo "installed already, skipping: $destination"
+    elif [[ ! -e $destination || $FORCE_INSTALL ]]; then
+        echo "symlinking: $destination -> $source"
+        mkdir -p "$(dirname "$destination")"
+        ln -sf "$source" "$destination"
+    else
+        echo "exists already, skipping: $destination"
+    fi
+}
+
 install_dotfile_by_copying() {
     local source destination
 
@@ -85,19 +124,7 @@ install_dotfile_by_copying() {
 
     local file
     for file in "$@"; do
-        source="$SOURCE_DIR/$file"
-        destination=~/"$file"
-
-        if [[ -f $destination ]] && cmp -s "$source" "$destination"; then
-            echo "installed already, skipping: $destination"
-        elif [[ ! -e $destination || $force_install ]]; then
-            echo "copying: $destination -> $source"
-            mkdir -p "$(dirname "$destination")"
-            rm -f "$destination" # Delete potential destination file that is a symlink to avoid following it
-            cp "$source" "$destination"
-        else
-            echo "exists already, skipping: $destination"
-        fi
+        install_copy "$SOURCE_DIR/$file" ~/"$file"
     done
 }
 
@@ -108,22 +135,24 @@ install_dotfile_by_symlinking() {
 
     local file
     for file in "$@"; do
-        source="$SOURCE_DIR/$file"
-        destination=~/"$file"
-
-        if [[ -L $destination && $source -ef $destination ]]; then
-            echo "installed already, skipping: $destination"
-        elif [[ ! -e $destination || $force_install ]]; then
-            echo "symlinking: $destination -> $source"
-            mkdir -p "$(dirname "$destination")"
-            ln -sf "$source" "$destination"
-        else
-            echo "exists already, skipping: $destination"
-        fi
+        install_symlink "$SOURCE_DIR/$file" ~/"$file"
     done
 }
 
-force_install=
+install_symlinks() {
+    local -n asso_array_ref
+
+    [[ $# -le 0 || -z $1 ]] && print_error "install_symlinks(): expects reference to associated array name, where the array maps destination file paths to source file paths" && return 1
+
+    asso_array_ref=$1
+
+    local destination
+    for destination in "${!asso_array_ref[@]}"; do
+        install_symlink "${asso_array_ref[$destination]}" "$destination"
+    done
+}
+
+FORCE_INSTALL=
 
 while :; do
     case ${1:-} in
@@ -132,7 +161,7 @@ while :; do
             exit
             ;;
         -f | --force)
-            force_install=1
+            FORCE_INSTALL=1
             ;;
         *)
             break
@@ -147,4 +176,5 @@ install_dotfile_by_symlinking "${INSTALL_DOTFILE_BY_SYMLINKING_COMMON[@]}"
 if [[ $(uname) == "Darwin" ]]; then
     install_dotfile_by_copying "${INSTALL_DOTFILE_BY_COPYING_DARWIN[@]}"
     install_dotfile_by_symlinking "${INSTALL_DOTFILE_BY_SYMLINKING_DARWIN[@]}"
+    install_symlinks INSTALL_SYMLINKS_DARWIN
 fi
